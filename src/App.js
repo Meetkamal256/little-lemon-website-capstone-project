8a923d9d690bf1659661f2f72d4d12d92f035cac
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-04-16 11:19:43 +0100

    reformatted  code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import "./index.css";
+import React from "react";
+import "./App.css";
 import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
 import Footer from "./components/Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Nav />
-        <Footer />
-        
-        
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import logo from "../images/Logo.svg"
-
+import React, { useState } from "react";
+import logo from "../images/Logo .svg";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <nav>
-      <a href="/"> <img src={logo} alt="logo" /></a>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Menu</li>
-        <li>Reservations</li>
-        <li>Order online</li>
-        <li>Login</li>
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+
+      {/* mobile navbar */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      {/* nav items */}
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
       </ul>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
